592f5c99a593a0f002fad1d19b618bf25be089a5
Ulil Fahmi <ulilf442@gmail.com>
2026-05-06 10:39:21 +0700

    update input preferensi

---
+++ .figma/1_1744/index.jsx
@@ -0,0 +1,223 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <div className={styles.halamanInputReferens}>
+      <div className={styles.main}>
+        <div className={styles.topNavigationBar}>
+          <p className={styles.jelajahBali}>Jelajah Bali</p>
+          <div className={styles.container}>
+            <p className={styles.text}>Destinations</p>
+            <div className={styles.link}>
+              <p className={styles.text2}>Curations</p>
+            </div>
+            <p className={styles.text}>About</p>
+          </div>
+          <div className={styles.buttonCssTransform}>
+            <div className={styles.button} />
+          </div>
+        </div>
+        <div className={styles.autoWrapper3}>
+          <div className={styles.editorialSideIntrodu}>
+            <div className={styles.container3}>
+              <p className={styles.text3}>RECOMMENDATION ENGINE</p>
+              <div className={styles.heading1}>
+                <p className={styles.craftYourPerfectBali3}>
+                  <span className={styles.craftYourPerfectBali}>
+                    Craft Your&nbsp;
+                  </span>
+                  <span className={styles.craftYourPerfectBali2}>
+                    Perfect
+                    <br />
+                    Bali
+                  </span>
+                  <span className={styles.craftYourPerfectBali}>&nbsp;Escape.</span>
+                </p>
+              </div>
+              <div className={styles.container2}>
+                <p className={styles.text4}>
+                  Our precision curation engine transforms your preferences
+                  <br />
+                  into a tailored itinerary. Select your sanctuary, set your
+                  <br />
+                  pace, and let the horizon reveal itself.
+                </p>
+              </div>
+            </div>
+            <div className={styles.overlayBlur}>
+              <div className={styles.overlayShadow}>
+                <img
+                  src="../image/mos268mh-pt07rre.png"
+                  className={styles.aB6AXuCUzlPqJ6DxzJq1}
+                />
+                <div className={styles.background}>
+                  <div className={styles.container5}>
+                    <img
+                      src="../image/mos268mb-gnjc7oh.svg"
+                      className={styles.container4}
+                    />
+                    <p className={styles.text5}>Ubud Highlands</p>
+                  </div>
+                  <p className={styles.curatedNatureRetreat}>
+                    Curated Nature Retreats
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.functionalSideThePre}>
+            <div className={styles.subtleAccentBar} />
+            <div className={styles.form}>
+              <div className={styles.locationSelection}>
+                <div className={styles.label}>
+                  <img
+                    src="../image/mos268mb-l3nqqp7.svg"
+                    className={styles.container6}
+                  />
+                  <p className={styles.text6}>Where do you want to explore?</p>
+                </div>
+                <div className={styles.options}>
+                  <div className={styles.imageClip}>
+                    <img
+                      src="../image/mos268mb-mql2tn6.svg"
+                      className={styles.sVg}
+                    />
+                    <p className={styles.selectARegency}>Select a Regency</p>
+                    <img
+                      src="../image/mos268mb-3u14ks3.svg"
+                      className={styles.container7}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.categorySelectionChi}>
+                <div className={styles.label2}>
+                  <img
+                    src="../image/mos268mb-dab7w9w.svg"
+                    className={styles.container8}
+                  />
+                  <p className={styles.text6}>Primary Interest</p>
+                </div>
+                <div className={styles.container9}>
+                  <div className={styles.background2}>
+                    <p className={styles.text7}>Nature</p>
+                  </div>
+                  <div className={styles.background3}>
+                    <p className={styles.text8}>Culture</p>
+                  </div>
+                  <div className={styles.background3}>
+                    <p className={styles.text8}>Beach</p>
+                  </div>
+                  <div className={styles.background3}>
+                    <p className={styles.text8}>Culinary</p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.budgetSlider}>
+                <div className={styles.container11}>
+                  <div className={styles.label3}>
+                    <img
+                      src="../image/mos268mb-zgiezb6.svg"
+                      className={styles.container10}
+                    />
+                    <p className={styles.text6}>Daily Budget</p>
+                  </div>
+                  <div className={styles.background4}>
+                    <p className={styles.text9}>IDR 500k - 2M+</p>
+                  </div>
+                </div>
+                <div className={styles.container14}>
+                  <div className={styles.input}>
+                    <div className={styles.container12}>
+                      <div className={styles.backgroundBorderShad} />
+                    </div>
+                  </div>
+                  <div className={styles.container13}>
+                    <p className={styles.text10}>BUDGET</p>
+                    <p className={styles.text10}>MID-RANGE</p>
+                    <p className={styles.text10}>LUXURY</p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.facilitiesCheckboxes}>
+                <div className={styles.label4}>
+                  <img
+                    src="../image/mos268mb-g9rqp6i.svg"
+                    className={styles.container15}
+                  />
+                  <p className={styles.text6}>Essential Amenities</p>
+                </div>
+                <div className={styles.container16}>
+                  <div className={styles.autoWrapper}>
+                    <div className={styles.label5}>
+                      <div className={styles.input2} />
+                      <p className={styles.text11}>Parking Area</p>
+                    </div>
+                    <div className={styles.label6}>
+                      <div className={styles.input2} />
+                      <p className={styles.text11}>Clean Restrooms</p>
+                    </div>
+                  </div>
+                  <div className={styles.autoWrapper2}>
+                    <div className={styles.label7}>
+                      <div className={styles.input3}>
+                        <img
+                          src="../image/mos268mb-1n7mmsz.svg"
+                          className={styles.sVg2}
+                        />
+                      </div>
+                      <p className={styles.text11}>High-speed Wifi</p>
+                    </div>
+                    <div className={styles.label6}>
+                      <div className={styles.input2} />
+                      <p className={styles.text11}>On-site Restaurant</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.primaryAction}>
+                <div className={styles.button2}>
+                  <div className={styles.buttonShadow}>
+                    <p className={styles.text12}>Cari Rekomendasi</p>
+                    <img
+                      src="../image/mos268mb-dpv3btu.svg"
+                      className={styles.container17}
+                    />
+                  </div>
+                </div>
+                <p className={styles.text13}>
+                  Powered by Curated Horizon AI • Personalizing 4,200+ Bali
+                  Destinations
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.container18}>
+          <p className={styles.jelajahBali2}>Jelajah Bali</p>
+          <p className={styles.a2026Kelompok3}>© 2026 kelompok 3</p>
+        </div>
+        <div className={styles.container19}>
+          <div className={styles.link2}>
+            <p className={styles.text14}>PRIVACY POLICY</p>
+          </div>
+          <div className={styles.link2}>
+            <p className={styles.text14}>TERMS OF SERVICE</p>
+          </div>
+          <div className={styles.link2}>
+            <p className={styles.text14}>CONTACT SUPPORT</p>
+          </div>
+          <div className={styles.link3}>
+            <p className={styles.text15}>BALI GUIDE</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Component;
