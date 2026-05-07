113fdaf0f4b6ff49383c085c2494499f8bdca60b
Ulil Fahmi <ulilf442@gmail.com>
2026-05-07 16:41:26 +0700

    Restore destination-specific styles in layout to fix /destinations visuals

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,9 @@
     {{-- Tailwind CDN --}}
     <script src="https://cdn.tailwindcss.com"></script>
 
+    {{-- App assets --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     {{-- Alpine --}}
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
