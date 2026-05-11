beee07701e24ecdd9a088485a4cd50c481757cf3
= <=>
2026-05-11 11:10:14 +0700

    fix routing landing page

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\LandingPageController;
 
 /*
