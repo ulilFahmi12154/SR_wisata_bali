f0809a753c7dc6116634136bbbc4d4df66acac92
snowvann <danicanasywa5a@gmail.com>
2026-06-01 19:59:16 +0700

    memperbaiki nama form

--- resources/views/pages/admin/users/index.blade.php
+++ resources/views/pages/admin/users/index.blade.php
@@ -44,7 +44,7 @@
                     <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M12 4v16m8-8H4"/>
                     </svg>
-                    <span>Tambah User Baru</span>
+                    <span>Tambah Pengguna Baru</span>
                 </button>
             </div>
 
