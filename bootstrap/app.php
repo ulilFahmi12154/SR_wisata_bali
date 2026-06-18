48035dc715694d0de4550b698593211df40a3b59
Ulil Fahmi <ulilf442@gmail.com>
2026-06-18 14:03:10 +0700

    fix(deploy): paksa URL form memakai HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +12,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(
+            at: env('TRUSTED_PROXIES', '*'),
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT |
+                Request::HEADER_X_FORWARDED_PROTO |
+                Request::HEADER_X_FORWARDED_PREFIX |
+                Request::HEADER_X_FORWARDED_AWS_ELB
+        );
+
         $middleware->redirectGuestsTo(fn ($request) => $request->is('admin/*')
             ? route('admin.login')
             : route('user.login'));
